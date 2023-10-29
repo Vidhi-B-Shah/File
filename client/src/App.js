3ac61703ced37beca24bdda4121bde148e7934bd
Vedantika Singh <vedantikasingh@Vedantikas-MacBook-Air.local>
2023-10-29 15:15:04 +0530

    final

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,31 @@
+import React from 'react';
 import './App.css';
 import Navbar from './Navbar';
-import {useRef, useState} from 'react';
+import {useRef, useState, useEffect} from 'react';
+import { uploadFile } from './services/api';
 
 
 function App() {
   const [file, setFile] = useState(' ');
+  const [result, setResult] = useState('');
   const fileInputRef = useRef();
+
+  useEffect(() => {
+    const getImage = async () => {
+      if (file) {
+        const data = new FormData();
+        data.append("name", file.name);
+        data.append("file", file);
+
+        let response = await uploadFile(data);
+        setResult(response.path);
+
+      }
+
+    }
+    getImage();
+
+  },[file])
   const onUploadClick= () => {
     fileInputRef.current.click();
   }
@@ -20,6 +40,8 @@ function App() {
         ref = {fileInputRef}
         style = {{display : 'none'}}
         onChange={(e)=> setFile(e.target.files[0])}/>
+
+        <a href={result} target= "_blank">{result}</a>
       </div>
     </div>
   );
